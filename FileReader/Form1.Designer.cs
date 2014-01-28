6352c80932c9ac9bb3c1b89acc2fb817c20676e3
ahinger <andrewjhinger@gmail.com>
2014-01-28 14:06:25 -0500

    hg

---
+++ FileReader/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace FileReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.exceptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noneCheckBox = new System.Windows.Forms.CheckBox();
+            this.throwCheckBox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.exceptionsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 238);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // exceptionsToolStripMenuItem
+            // 
+            this.exceptionsToolStripMenuItem.Name = "exceptionsToolStripMenuItem";
+            this.exceptionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exceptionsToolStripMenuItem.Text = "Exceptions";
+            // 
+            // noneCheckBox
+            // 
+            this.noneCheckBox.AutoSize = true;
+            this.noneCheckBox.Checked = true;
+            this.noneCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.noneCheckBox.Location = new System.Drawing.Point(192, 1);
+            this.noneCheckBox.Name = "noneCheckBox";
+            this.noneCheckBox.Size = new System.Drawing.Size(52, 17);
+            this.noneCheckBox.TabIndex = 3;
+            this.noneCheckBox.Text = "None";
+            this.noneCheckBox.UseVisualStyleBackColor = true;
+            this.noneCheckBox.CheckedChanged += new System.EventHandler(this.noneCheckBox_CheckedChanged);
+            // 
+            // throwCheckBox
+            // 
+            this.throwCheckBox.AutoSize = true;
+            this.throwCheckBox.Location = new System.Drawing.Point(134, 1);
+            this.throwCheckBox.Name = "throwCheckBox";
+            this.throwCheckBox.Size = new System.Drawing.Size(56, 17);
+            this.throwCheckBox.TabIndex = 4;
+            this.throwCheckBox.Text = "Throw";
+            this.throwCheckBox.UseVisualStyleBackColor = true;
+            this.throwCheckBox.CheckedChanged += new System.EventHandler(this.throwCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.throwCheckBox);
+            this.Controls.Add(this.noneCheckBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem exceptionsToolStripMenuItem;
+        private System.Windows.Forms.CheckBox noneCheckBox;
+        private System.Windows.Forms.CheckBox throwCheckBox;
+    }
+}
+
---
+++ FileReader/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace FileReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    if (throwCheckBox.Checked == true)
+                    {
+                        throw new MyFileException("Custom file exception");
+                    }
+                    else
+                    {
+
+                        String input = string.Empty;
+                        OpenFileDialog dialog = new OpenFileDialog();
+                        dialog.Filter =
+                                 "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                        dialog.InitialDirectory = "C:";
+                        dialog.Title = "Select a text file";
+
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            richTextBox1.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+                            toolStripStatusLabel1.Text = dialog.SafeFileName;
+                        }
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    // Write error.
+                    MessageBox.Show(ex.Message);
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            catch (MyFileException Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                Console.WriteLine(Ex.ToString());
+            }
+        }
+
+
+        private void CustomFileExceptions()
+        {
+            throw new MyFileException("Test custom file exception");
+        }
+
+        class MyFileException : ApplicationException
+        {
+            public MyFileException(string exceptionText) : base(exceptionText) { }
+            public MyFileException(string exceptionText, Exception innerException) : base(exceptionText, innerException) { }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void throwCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (throwCheckBox.Checked == true)
+            {
+                noneCheckBox.Checked = false;
+            }
+        }
+
+        private void noneCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (noneCheckBox.Checked == true)
+            {
+                throwCheckBox.Checked = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
